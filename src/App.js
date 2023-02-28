18a30c46d6914da486fc6b77cfad2e82a040b1cb
YouYou94 <ybh4115@gmail.com>
2023-02-28 12:02:26 +0900

    리액트 라우터 돔 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
